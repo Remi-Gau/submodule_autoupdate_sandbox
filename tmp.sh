e3ac2fc2d8ed5008eb96e17c07ce3609959a15c3
Remi Gau <remi_gau@hotmail.com>
2022-05-12 16:38:28 +0200

    updating git will solve everything

--- tmp.sh
+++ tmp.sh
@@ -2,27 +2,24 @@
 
 set -e
 
-start_dir=$PWD
-SUBMOD_TO_UPDATE="lib/sub_3 lib/sub_1"
+SUBMOD_TO_UPDATE="*"
 
+set -e -u -x
+git submodule update --init --recursive
 if [ "${SUBMOD_TO_UPDATE}" = "*" ]; then
     submodules=$(git submodule | awk '{print $2}')
 else
     submodules=$(echo -e ${SUBMOD_TO_UPDATE} | sed "s/ /\n/g")
 fi
-
 nb_submod=$(echo "${submodules}" | wc -l)
-
 echo -e "\nUPDATING ${nb_submod} SUBMODULES"
 echo -e "${submodules}"
-
 for i in $(seq 1 ${nb_submod}); do
     path=$(echo -e ${submodules} | awk -v i=${i} '{print $i}')
     branch=$(git config --get --file .gitmodules submodule.${path}.branch)
     echo -e "\nswitching submodule ${path} to ${branch}"
-    cd "${path}" || exit
-    git checkout ${branch}
-    cd "${start_dir}"
+    git -C ${path} fetch --depth=1 origin "refs/heads/${branch}"
+    git -C ${path} branch -a
+    git -C ${path} checkout "remotes/origin/${branch}"
 done
-
 git submodule update --remote --merge
